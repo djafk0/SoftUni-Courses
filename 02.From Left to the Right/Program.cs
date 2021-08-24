c6a35c92e020116969ae6af41188bf0f4b8fb8fd
djafk0 <zdravkomestendikov@gmail.com>
2021-08-24 19:52:39 +0300

    Second try

---
+++ 02.From Left to the Right/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.From_Left_to_the_Right
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                string left = Console.ReadLine();
+                string right = Console.ReadLine();
+
+            }
+        }
+    }
+}
