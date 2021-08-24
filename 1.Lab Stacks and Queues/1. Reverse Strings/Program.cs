a7107faf976582f7476d8099f94be12635b517ef
djafk0 <zdravkomestendikov@gmail.com>
2021-08-24 20:28:04 +0300

    Stacks and Queues

---
+++ 1.Lab Stacks and Queues/1. Reverse Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> reversed = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                reversed.Push(input[i]);
+            }
+
+            Console.WriteLine(string.Join("", reversed));            
+        }
+    }
+}
