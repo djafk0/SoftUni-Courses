19c1960753693c64d7e4d7e0be97247098e8373e
djafk0 <zdravkomestendikov@gmail.com>
2021-08-26 16:10:15 +0300

    Stacks and Queues exercises

---
+++ Advanced/1.ExerciseStacksAndQueues/4.FastFood/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+            Queue<int> numbers = new Queue<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Max());
+            }
+
+            if (numbers.Sum() <= food)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                while (true)
+                {
+                    int curr = numbers.Peek();
+                    if (food - curr >= 0)
+                    {
+                        food -= numbers.Dequeue();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                
+                Console.WriteLine($"Orders left: {string.Join(' ', numbers)}");
+            }
+        }
+    }
+}
---
+++ Advanced/1.ExerciseStacksAndQueues/6.SongsQueue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries));
+            string command = Console.ReadLine();
+
+            while (songs.Count != 0)
+            {
+                if (command == "Play")
+                {
+                    songs.Dequeue();
+                }
+                else if (command.Substring(0, 3) == "Add")
+                {
+                    if (songs.Contains(command.Substring(4)))
+                    {
+                        Console.WriteLine($"{command.Substring(4)} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(command.Substring(4));
+                    }
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
