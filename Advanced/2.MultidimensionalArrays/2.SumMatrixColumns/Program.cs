a1f987fb79d51e7ae6481d5a822e3e0c96864a52
djafk0 <zdravkomestendikov@gmail.com>
2021-08-27 15:26:00 +0300

    Lab

---
+++ Advanced/2.MultidimensionalArrays/2.SumMatrixColumns/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] demension = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] matrix = new int[demension[0], demension[1]];
+
+            for (int row = 0; row < demension[0]; row++)
+            {
+                int[] curr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < demension[1]; col++)
+                {
+                    matrix[row, col] = curr[col];
+                }
+            }
+
+
+            for (int col = 0; col < demension[1]; col++)
+            {
+                int sum = 0;
+
+                for (int row = 0; row < demension[0]; row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+            }
+
+        }
+    }
+}
