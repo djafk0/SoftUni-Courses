5b51b438abfd57801fcd4bdfcfc16e3f1c4f2560
djafk0 <zdravkomestendikov@gmail.com>
2021-08-30 16:48:54 +0300

    Exercise

---
+++ Advanced/2.1MultidimensionalArrays/2.2X2SquaresInMatrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._2X2SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            string[,] matrix = new string[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] currMatrix = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = currMatrix[j];
+                }
+            }
+
+            int counter = 0;
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    if (matrix[i, j] == matrix[i, j + 1] && matrix[i, j] == matrix[i + 1, j] && matrix[i, j] == matrix[i + 1, j + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ Advanced/2.1MultidimensionalArrays/6.JaggedArrayManipulator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace _6.JaggedArrayManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[][] jagged = new double[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                double[] curr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+                jagged[i] = curr;
+
+                if (i != 0)
+                {
+                    if (jagged[i].Length == jagged[i - 1].Length)
+                    {
+                        for (int j = 0; j < jagged[i].Length; j++)
+                        {
+                            jagged[i][j] = jagged[i][j] * 2;
+                            jagged[i - 1][j] = jagged[i - 1][j] * 2;
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 0; j < jagged[i - 1].Length; j++)
+                        {
+                            jagged[i - 1][j] = jagged[i - 1][j] / 2;
+                        }
+
+                        for (int j = 0; j < jagged[i].Length; j++)
+                        {
+                            jagged[i][j] = jagged[i][j] / 2;
+                        }
+                    }
+                }
+            }
+
+            string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "End")
+            {
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                int value = int.Parse(command[3]);
+
+                if (!(row < 0 || row >= n || col < 0 || col >= jagged[row].Length))
+                {
+                    if (command[0] == "Add")
+                    {
+                        jagged[row][col] += value;
+                    }
+                    else if (command[0] == "Subtract")
+                    {
+                        jagged[row][col] -= value;
+                    }
+                }
+
+                command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var item in jagged)
+            {
+                Console.WriteLine(string.Join(' ', item));
+            }
+        }
+    }
+}
