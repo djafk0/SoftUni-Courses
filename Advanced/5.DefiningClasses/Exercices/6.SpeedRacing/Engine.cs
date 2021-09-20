556b9be7637130ee827ff462068cf4b91677dd67
djafk0 <zdravkomestendikov@gmail.com>
2021-09-20 12:46:34 +0300

    Exercies uploaded

---
+++ Advanced/5.DefiningClasses/Exercices/6.SpeedRacing/Engine.cs
@@ -0,0 +1,9 @@
+﻿namespace _6.SpeedRacing
+{
+    public class Engine
+    {
+        public int EngineSpeed { get; set; }
+        public int EnginePower { get; set; }
+
+    }
+}
---
+++ Advanced/5.DefiningClasses/Exercices/DefiningClasses/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            int days = DateModifier.DateBetweenTwoDatesInDays(firstDate, secondDate);
+            Console.WriteLine($"{Math.Abs(days)}");
+        }
+    }
+}
