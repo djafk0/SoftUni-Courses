fda2fb1d1e7f760fa5968b8b7e5df83828aab6b9
djafk0 <zdravkomestendikov@gmail.com>
2021-11-08 20:36:49 +0200

    encapsulation lab upload

---
+++ 4.OOP/2.Encapsulation-Lab/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string player = Console.ReadLine();
+                string[] playerSplit = player.Split(' ');
+                Person person = new Person(playerSplit[0], playerSplit[1], int.Parse(playerSplit[2]), decimal.Parse(playerSplit[3]));
+                persons.Add(person);
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
---
+++ 4.OOP/2.Encapsulation-Lab/Team.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+        public string Name { get; set; }
+
+        public IReadOnlyList<Person> FirstTeam => this.firstTeam.AsReadOnly();
+
+        public IReadOnlyList<Person> ReserveTeam => this.reserveTeam.AsReadOnly();
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
