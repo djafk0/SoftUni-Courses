85438ac663451f542394fdfecbe6566f4ba48bfc
djafk0 <zdravkomestendikov@gmail.com>
2021-12-12 11:13:14 +0200

    inheritance exercises uploaded

---
+++ 4.OOP/1.Inheritance/PlayersAndMonsters/Elf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Elf : Hero
+    {
+        public Elf(string username, int level) : base(username, level)
+        {
+        }
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
